b70e352b598b10737d4cfebdc74e641eaa21a278
Chris Ball <chrisball7@gmail.com>
2019-06-14 05:11:11 -0700

    adjusted settings.py to use postgres, added clients project to installed apps, added allowed hosts

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,40 @@
-FROM python:3.7-slim
+# FROM python:3.7-slim
 
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+# ENV PYTHONDONTWRITEBYTECODE 1
+# ENV PYTHONUNBUFFERED 1
 
-RUN mkdir /src
-WORKDIR /src
+# RUN mkdir /src
+# WORKDIR /src
 
-RUN apt-get update
+# RUN apt-get update
 
-COPY . /src
+# COPY . /src
 
-RUN pip install -r requirements.txt
+# RUN pip install -r requirements.txt
+
+# -----
+
+#Grab the latest alpine image
+FROM alpine:latest
+
+# Install python and pip
+RUN apk add --no-cache --update python3 py3-pip bash
+ADD ./requirements.txt /tmp/requirements.txt
+
+# Install dependencies
+RUN pip3 install --no-cache-dir -q -r /tmp/requirements.txt
+
+# Add our code
+ADD ./ /opt/webapp/
+WORKDIR /opt/webapp
+
+# Expose is NOT supported by Heroku
+# EXPOSE 5000 		
+
+# Run the image as a non-root user
+RUN adduser -D myuser
+USER myuser
+
+# Run the app.  CMD is required to run on Heroku
+# $PORT is set by Heroku			
+CMD gunicorn --bind 0.0.0.0:$PORT wsgi 
--- clients/settings.py
+++ clients/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iuyt#-xzd-0ufb3ci($ltqr$glq1_1jh9+*$you2d749j(tt(k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clients',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'clients.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': 5432,
+        'TEST': {
+            'NAME': 'clients_test'
+        }
     }
 }
 
